d09b2b827449f440084081ecd7c901aa74d0416b
NURIKZIX <nurzigitrashitov1234567@gmail.com>
2026-06-22 16:55:03 +0500

    Firebase конфигурациясын .env файлына көшіру

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,30 +1,21 @@
 // lib/firebase.ts
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase конфигурациясы
 const firebaseConfig = {
-  apiKey: "AIzaSyAYTn7of45rZbeD4X6IlFr-Q-DHV_ZV2d0",
-  authDomain: "qazmura-3091a.firebaseapp.com",
-  projectId: "qazmura-3091a",
-  storageBucket: "qazmura-3091a.firebasestorage.app",
-  messagingSenderId: "329284352850",
-  appId: "1:329284352850:web:b87e88260ce83836bad88b",
-  measurementId: "G-8WE8F06JH3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Firebase-ті тек бір рет іске қосу
-const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-// Authentication
 export const auth = getAuth(app);
-
-// Firestore Database
 export const db = getFirestore(app);
 
-// Firebase App
 export default app;
